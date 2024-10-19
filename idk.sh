3517863ff5ed857bc64239dfb91569173103de90
katy-the-kat <hello@is-a.space>
2024-10-19 18:08:54 +0300

    Update idk.sh

--- idk.sh
+++ idk.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-apt install qemu-system libvirt-clients libvirt-daemon-system cockpit cockpit-machines neofetch virt-manager 
-apt install --no-install-recommends xfce4 xfce4-terminal
+apt install qemu-system libvirt-clients libvirt-daemon-system cockpit cockpit-machines neofetch virt-manager -y
+apt install --no-install-recommends xfce4 xfce4-terminal -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo wget -O /var/lib/libvirt/images/alpine.iso https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-virt-3.20.3-x86_64.iso
+wget -O /var/lib/libvirt/images/alpine.iso https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-virt-3.20.3-x86_64.iso
 apt install -y ./chrom*
 sudo fallocate -l 192G /swapfile
 sudo chmod 600 /swapfile
