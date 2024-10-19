e60c2c14c02464a3c608e0e61a5771659123b323
katy-the-kat <hello@is-a.space>
2024-10-19 18:09:40 +0300

    Update idk.sh

--- idk.sh
+++ idk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install qemu-system libvirt-clients libvirt-daemon-system cockpit cockpit-machines neofetch virt-manager -y
+apt install qemu-system libvirt-clients libvirt-daemon-system neofetch virt-manager -y
 apt install --no-install-recommends xfce4 xfce4-terminal -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 wget -O /var/lib/libvirt/images/alpine.iso https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-virt-3.20.3-x86_64.iso
