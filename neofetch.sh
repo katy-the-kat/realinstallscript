5263f00e2732f0022fa66ec98fa1ab4db3ce271d
katy-the-kat <hello@is-a.space>
2024-10-20 18:31:49 +1400

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -2533,7 +2533,7 @@ get_memory() {
         ;;
     esac
 
-    memory="${mem_used}${mem_label:-MiB} / ${mem_total}${mem_label:-MiB} ${mem_perc:+(${mem_perc}%)} (Memory info is mirrored from node)"
+    memory="${mem_used}${mem_label:-MiB} / ${mem_total}${mem_label:-MiB} ${mem_perc:+(${mem_perc}%)} (Info mirrored from node)"
 
     # Bars.
     case $memory_display in
