630685d0d9ca425938bf7a8ffa2d0a8257a8109f
Katy <hello@is-a.space>
2025-01-29 15:16:26 -0600

    Create setup3.sh

---
+++ setup3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+apk update
+apk add nano docker bc bash neofetch
+
+rc-update add docker boot
+service docker start
+
+cat <<EOF > Dockerfile
+FROM ubuntu:22.04
+
+RUN apt-get update 
+RUN apt-get install -y nano htop wget dialog openssh-server openssh-client sudo tmate
+RUN wget -O /usr/bin/port https://raw.githubusercontent.com/katy-the-kat/portip/refs/heads/main/pubb.sh
+RUN chmod +x /usr/bin/port
+RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+RUN echo 'root:root' | chpasswd
+RUN printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
+RUN apt-get install -y systemd systemd-sysv dbus dbus-user-session
+RUN printf "systemctl start systemd-logind" >> /etc/profile
+
+ENTRYPOINT ["/sbin/init"]
+EOF
+
+docker build -t utmp .
+
+clear && echo done
