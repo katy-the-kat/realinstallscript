cbdba8bbe19c3dfd3f9bdaae3910830265b0a933
Katy <hello@is-a.space>
2025-01-30 10:01:58 -0600

    Create setup-3-ubuntu.sh

---
+++ setup-3-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apt install nano docker.io -y
+
+cat <<EOF > Dockerfile
+FROM ubuntu:22.04
+
+RUN apt-get update 
+RUN apt-get install -y nano htop wget dialog openssh-server openssh-client sudo tmate
+RUN wget -O /usr/bin/port https://raw.githubusercontent.com/katy-the-kat/portip/refs/heads/main/pubb.sh
+RUN chmod +x /usr/bin/port
+RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+RUN echo 'root:root' | chpasswd
+RUN printf '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d
+RUN apt-get install -y systemd systemd-sysv dbus dbus-user-session
+RUN printf "systemctl start systemd-logind" >> /etc/profile
+
+ENTRYPOINT ["/sbin/init"]
+EOF
+
+docker build -t utmp .
+
+clear && echo done
