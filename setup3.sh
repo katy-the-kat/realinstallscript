ede4f93ac03c669ee69232a2eb591b51291b5618
Katy <hello@is-a.space>
2025-01-30 18:26:01 -0600

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -11,10 +11,8 @@ FROM ubuntu:22.04
 
 #RUN sed -i -e 's|http://[^ ]*archive.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' -e 's|http://[^ ]*security.ubuntu.com/ubuntu|http://my.archive.ubuntu.com/ubuntu|g' /etc/apt/sources.list  
 RUN apt-get update 
-RUN yes | apt-get full-upgrade
 RUN yes | apt-get install -y nano htop wget dialog openssh-server openssh-client sudo tmate snapd dialog
 RUN yes | unminimize
-RUN yes | apt-get full-upgrade -y
 RUN wget -O /usr/bin/port https://raw.githubusercontent.com/katy-the-kat/realinstallscript/refs/heads/main/ipv4x
 RUN chmod +x /usr/bin/port
 RUN sed -i 's/^#\?\s*PermitRootLogin\s\+.*/PermitRootLogin yes/' /etc/ssh/sshd_config
